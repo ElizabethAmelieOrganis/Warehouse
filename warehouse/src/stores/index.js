5155d1af19b30087e04865a84ab3955768dd737a
ElizabethAmelieOrganis <1119346628@qq.com>
2025-03-15 20:12:50 +0800

    pinia_test1.0

--- warehouse/src/stores/index.js
+++ warehouse/src/stores/index.js
@@ -1,5 +1,7 @@
 import { defineStore } from "pinia";
-//options store
+import { computed, ref } from "vue";
+//使用const方法定义后还不是仓库而是函数，需要使用时才变成仓库
+//options store（选项式）
 export const useUserStore = defineStore("basemessage", {
   state: () => {
     //相当于data 防止数据污染
@@ -18,4 +20,18 @@ export const useUserStore = defineStore("basemessage", {
       this.username;
     },
   },
+});
+//setup store（组合式）
+export const useEmailStore = defineStore("email", () => {
+  //定义响应式数据，相当于在state中
+  const email = ref("1119346628@qq.com");
+  const getterEmail = computed(() => {
+    //相当于getters
+    return email.value;
+  });
+  function addEmail() {
+    //相当于actions
+    email.value = email.value;
+  }
+  return { email, getterEmail, addEmail }; //最后要暴露
 });
